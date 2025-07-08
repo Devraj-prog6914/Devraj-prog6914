1ceee3b57e14ff4c152e8341a4d704c365acd4da
Devraj Shinde <shindedevaraj0@gmail.com>
2025-07-08 13:23:35 +0530

    This files are frontend and Backend of a splash screen of our DriveGuard app

---
+++ app/src/main/java/com/example/mayapp/MainActivity3.kt
@@ -0,0 +1,25 @@
+package com.example.mayapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main3)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 500)
+
+    }
+}
